e01225ad5e78c83258aab4f2beeb5885174c98bc
UltWolf <UltWolf@DESKTOP-7AKD7J1>
2020-11-27 13:02:57 +0200

    Добавьте файлы проекта.

---
+++ PATCH/Controllers/ValuesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using PATCH.Models;
+using System.Collections.Generic;
+
+namespace PATCH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="patchDoc"></param>
+        /// <remarks> 
+        /// [
+        ///  {
+        ///    "OperationType": 0,
+        ///    "path": "customerName",
+        ///    "op": "replace",
+        ///    "value":"Hi"
+        ///  }
+        ///  for collection 
+        ///  [ { "path": "/orders", "op": "add", "value":[{"OrderName":"Hi" } ]} ] 
+        ///  Больше информации, как и что отправлять
+        ///  https://sookocheff.com/post/api/understanding-json-patch/
+        ///]
+        ///</remarks>
+        /// <returns></returns>
+        [HttpPatch]
+    public IActionResult JsonPatchWithModelState(
+      [FromBody] JsonPatchDocument<Customer> patchDoc)
+    {
+        if (patchDoc != null)
+        {
+            var customer = new Customer() { CustomerName = "blah  blah", Orders = new List<Order>() { new Order() { OrderName = "filed" } } };
+
+            patchDoc.ApplyTo(customer, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return new ObjectResult(customer);
+        }
+        else
+        {
+            return BadRequest(ModelState);
+        }
+    }
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
+}
---
+++ PATCH/Startup.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+namespace PATCH
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "FoodFlow API",
+                    Description = "FoodFlow server API.",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "VReal Soft",
+                        Email = "andrey.s@vrealsoft.com",
+                        Url = new Uri("https://vrealsoft.com/", UriKind.Absolute),
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "Use under License",
+                        Url = new Uri("https://vrealsoft.com/", UriKind.Absolute)
+                    }
+                });
+                #region IncludeAllDocumentary
+
+                var xmlAssembly = Path.Combine(AppContext.BaseDirectory, "Patch.xml");
+                //var xmlBLL = Path.Combine(AppContext.BaseDirectory, "FoodFlow.BLL.xml");
+                //var xmlDAL = Path.Combine(AppContext.BaseDirectory, "FoodFlow.DAL.xml");
+
+                //options.IncludeXmlComments(xmlBLL);  
+                //options.IncludeXmlComments(xmlDAL);
+                options.IncludeXmlComments(xmlAssembly);
+
+                #endregion
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    In = ParameterLocation.Header,
+                    Description =
+                            "JWT Authorization header using the Bearer scheme. " +
+                            "\r\n\r\nEnter 'Bearer' [space] and then your token in the text input below." +
+                            "\r\n\r\nExample: \"Bearer ey...\"",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+                options.DescribeAllEnumsAsStrings();
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "FoodFlow API V1");
+            });
+            
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+        } 
+    }
+}
